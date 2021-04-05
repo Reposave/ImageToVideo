d5aeb6064af2477eaac76296ed3e43dc190319a3
Ardo Dlamini <ardodlamini@gmail.com>
2021-04-05 14:55:26 +0200

    Can now Read PGM Files.

--- extractor.cpp
+++ extractor.cpp
@@ -2,9 +2,18 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+int rows = 0;
+int columns= 0;
+
+int buffsize = 80;
 
 int main (int argc, char *argv[])
 {
+	std::string filename = argv[1];
+	
 	std::string x1 = argv[3];
 	//std::cout << x1 << std::endl;
 	std::string y1 = argv[4];
@@ -22,4 +31,69 @@ int main (int argc, char *argv[])
 		//std::cout << argv[i] << std::endl;
 		w.push_back(argv[i+1]);
 	}
+	
+	//Reading PGM File.
+	std::string line;
+	std::string previous; //Stores the previous line after an iteration.
+	
+   	std::ifstream infile("Images/"+filename);
+	std::stringstream ss;
+	
+	char buff[buffsize];
+	
+	while(line.compare("255")!=0){ //Iterate through the non-binary data.	
+		previous = line;
+		getline(infile, line);
+		
+		
+		//ss << buff;
+		//ss >> line;
+		//ss << "";
+		
+		std::cout << line << std::endl;
+	}
+	
+	getline(infile, line);//Go to next line with Binary data.
+
+	std::cout << previous << std::endl;
+	
+	ss.clear();
+	ss << previous;
+	
+	ss >> rows;
+	ss >> columns;
+	
+	//ss.getline(buff, 10,' ');
+	
+	//rows = atoi(buff);
+	
+	//ss.getline(buff, 10,' ');
+	
+	//columns = atoi(buff);
+	
+	std::cout << rows <<" "<< columns << " " << "columns" << std::endl;
+	
+	char array[rows][columns];
+	
+	ss << infile.rdbuf();
+	
+	for(int row = 0; row < rows; ++row){
+		for (int col = 0; col < columns; ++col){
+				ss >> array[row][col];
+			}
+    }
+    
+    infile.close();
+    ss.clear();
+    //print array
+  for(int row = 0; row < rows; ++row) {
+    for(int col = 0; col < columns; ++col) {
+      std::cout << array[row][col] << " ";
+    }
+    std::cout << std::endl;
+  }
+  
 }
+void readFile(std::string file){
+   //
+   }
